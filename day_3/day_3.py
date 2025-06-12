3a47b99e0a260d8d6d7b652383c09ad28074b179
UP240322 <up240322@alumnos.upa.edu.mx>
2025-06-11 23:48:15 -0600

    Trabajo terminado

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -3,17 +3,17 @@ heigth = 1.87
 complex_number = complex
 
 #4. Area of triangle
-base_triangle = 20
-heigth_triangle = 10
+base_triangle = float(input('introduce la base: '))
+heigth_triangle = float(input('introduce la altura: '))
 
 a_triangle = base_triangle * heigth_triangle * 0.5
 
 print('El área de un triángulo con base de',base_triangle,'y altura de',heigth_triangle,'es',a_triangle)
 
 #5. Perimeter of triangle
-sidea_triangle = 5
-sideb_triangle = 4
-sidec_triangle = 3
+sidea_triangle = float(input('introduce el lado a del triángulo: '))
+sideb_triangle = float(input('introduce el lado b del triángulo: '))
+sidec_triangle = float(input('introduce eñ lado c del triángulo: '))
 
 perimeter_triangle = sidea_triangle + sideb_triangle +sidec_triangle
 
@@ -30,7 +30,7 @@ print('El área de un rectángulo con base de',base_rectangle,'y altura de',heig
 
 #7. Area and circumference of a circle
 pi = 3.14
-radius = 3
+radius = float(input('introduce el radio: '))
 
 a_circle = pi * radius * radius 
 circum = 2 * pi * radius
@@ -39,8 +39,13 @@ print('El área del círculo es de',a_circle,'y su circunferencia es de',circum,
 
 #8. Slope and Euclidian distance
 
-x= 1
-func = 2 * x -2
+x1 = int(input('introduce el valor de x para el punto 1: '))
+y1 = 2 * x1 -2
+x2 = int(input('introduce el valor de x para el punto 2: '))
+y2 = 2 * x2 -2
+
+#9.Formulas 
+slope = (y2 - y1) / (x2 - x1)
 
 p1= (2,2)
 p2= (6,10)
@@ -49,7 +54,17 @@ sum= ((p2[0] - p1[0]) ** 2) + ((p2[1] - p1[1]) ** 2)
 
 dist = (sum) ** 0.5
 
-print('La distancia euclidiana de los puntos',p1, 'y',p2, 'es:',round(dist,2))
+#10. Comparison 
+print('La pendiente es de',slope, 'y la distancia euclidiana es de',round(dist,2))
+
+#11. Value of func
+x_11 = int(input('introduce el valor de x'))
+func = (x_11 * x_11) + (6 * x_11) + 9
+
+x_11_ex = 0
+func_ex = (x_11 * x_11) + (6 * x_11) + 9
+
+print('El valor de la función "y = x^2 + 6x + 9" cuando x vale',x_11,'es de',func,'y cuando vale',x_11_ex,'es de',func_ex)
 
 #12. Falsely comparison
 py = 'python'
@@ -88,13 +103,31 @@ else:
 f_div = 7 // 3
 
 str_number = '2.7'    
-convert_number = int(str_number)
+convert_number = float(str_number)
 
 print(f_div,'es igual a',convert_number, f_div == convert_number) 
 
+#19. Int equal Str 
+print(type('10') == type(10))
 
+#20. Int equal Int
+print(type(int(float('9.8'))) == type(10))
 
+#21. Pay
+hours = float(input("\nIngrese las horas trabajadas: "))
+rate = float(input("Ingrese la tarifa por hora: "))
+pay = hours * rate
+print(f"Su ganancia semanal es {pay}")
 
-#cambios repositorio para explicar
+#22. Years to sec
+years = int(input("\nIngrese cuántos años ha vivido: "))
+seconds_per_year = 365 * 24 * 60 *60
+total_seconds = years * seconds_per_year
+print(f"Usted ha vivido por {total_seconds} segundos.")
 
-#cambios otra vez xq no se veía el fokin cursor
+#23. Table
+print('1 1 1 1 1')
+print('2 1 2 4 8')
+print('3 1 3 9 27')
+print('4 1 4 16 64')
+print('5 1 5 25 125')
