b97622a7c872f043a2258e18cbaafe6d6e4432d2
up240322@alumnos.upa.edu.mx <up240322@alumnos.upa.edu.mx>
2025-07-04 11:31:47 -0600

    Level 3.3 Pendiente

--- day05.py
+++ day05.py
@@ -1,95 +1,76 @@
 # Day 5 
 
 # 1
-
 el = list()
 print(el)
 
-#puto 2
-
+# 2
 list_nums= ["1", "2", "3", "4", "5", "6"]
 
 # 3
-
 print(len(list_nums))
 
 # 4
-
 print(list_nums[0])
 print(list_nums[-1])
 middle = len(list_nums)//2
 print(list_nums[middle])
 
 # 5
-
 mixed_data_types = ["patricio", 19, 1.72, "single", "Aguascalientes"]
 
 # 6
-
 it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 
 # 7
-
 print(it_companies)
 
 # 8
-
 print(len(it_companies))
 
 # 9
-
 print(it_companies[0])
 print(it_companies[-1])
 middle = len(it_companies)//2
 print(it_companies[middle])
 
 # 10
-
 it_companies[1] = "gogle"
 print(it_companies)
 
 # 11
-
 it_companies.append("apple")
 print(it_companies)
 
 # 12
-
 middle = len(it_companies)//2
 it_companies.insert(middle,"intel")
 print(it_companies)
 
 # 13
-
 it_companies[4] = it_companies[4].upper()
 print(it_companies)
 
 # 14
-
 print("#; ".join(it_companies))
 
 # 15
-
 print("IBM" in it_companies)
 print("Google" in it_companies)
 
 # 16
-
 it_companies.sort()
 print(it_companies)
 
 # 17
-
 it_companies.reverse()
 print(it_companies)
 
 # 18
-
 first_3 = it_companies[:3]
 print(first_3)
 
 # 19
-
 last_3 = it_companies[-3:]
 print(last_3)
 
@@ -98,39 +79,32 @@ middle = len(it_companies)//2
 print(it_companies[middle:middle+1])
 
 # 21
-
 del it_companies[0]
 print(it_companies)
 
 # 22
-
 middle = len(it_companies)//2
 del it_companies [middle:middle+1]
 print(it_companies)
 
 # 23
-
 del it_companies[-1]
 print(it_companies)
 
 # 24
-
 del it_companies[:]
 print(it_companies)
 
 # 25
-
 del it_companies
 
 # 26
-
 front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 back_end = ['Node','Express', 'MongoDB']
 join = front_end + back_end
 print(f"{front_end} + {back_end}\n= {join}")
 
 # 27
-
 full_stack = join
 redux = full_stack.index("Redux")
 position = redux+1
--- day07.py
+++ day07.py
@@ -9,9 +9,44 @@ print(len(it_companies))
 it_companies.add('Twitter')
 
 it_companies.update(['Nvidia','Tesla'])
-
+ 
 it_companies.remove('Facebook')
 
 print(it_companies)
 
-#Prueba git desde otra lap 
+print("la diferencia de discard y remove es que si no existe el objeto, discard no saldrá con error")
+
+#------------ Level 2 ------------
+#Join A and B
+print(A.union(B),'\n')
+
+#Find A intersection B
+print("A intersection B:",A.intersection(B),'\n')
+
+#Is A subset of B
+print('Is A subset of B:', A.issubset(B),'\n')
+
+#Are A and B disjoint sets
+print('Is A and B disjoint:', A.isdisjoint(B),'\n')
+
+#Join A with B and B with A
+print('Join A with B',A.union(B),'\n')
+print('Join B with A',B.union(A),'\n')
+
+#What is the symmetric difference between A and B
+print('Symetric difference:',B.symmetric_difference(A),'\n')
+
+#Delete the sets completely
+del A,B
+
+#------------ Level 3 ------------ 
+#Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+print('Is the set bigger than the list?', len(set(age))  > len(age))
+
+#Explain the difference between the following data types: string, list, tuple and set
+print('Strings are sequences of caracters')
+print('Lists are ordered, mutable and can be duplicated')
+print('Tuples are ordered, inmutable and can be duplicated')
+print('Sets are unordered, unique elements with no duplications')
+
+#I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
